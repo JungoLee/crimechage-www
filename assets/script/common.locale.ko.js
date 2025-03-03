08e93859c5bbdf32066731de4adddc5937677a5e
명훈PC\이명훈 <audgns722@gmail.com>
2025-03-03 18:37:23 +0900

    업데이트

---
+++ assets/script/common.locale.ko.js
@@ -0,0 +1,17 @@
+// const etUI = {} || window.etUI;
+// etUI.locale.ko = {
+//   swiper: {
+//     next: '다음',
+//     prev: '이전'
+//   }
+// }
+//
+// etUI.locale.en = {
+//   swiper: {
+//     next: 'next',
+//     prev: 'prev'
+//   }
+// }
+//
+// etUI.locale.defaultLocale = 'ko';
+// etUI.locale.setLocale('ko')
